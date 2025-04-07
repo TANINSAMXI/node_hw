a3aaf2ade7fb1816aa82fd7459d94ceef39a5ec7
TANINSAMXI <pischaniyd@gmail.com>
2025-04-07 10:39:39 +0200

    hw_3 complete

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-const message = "Welcome to Node.js"
-alert(message)
+const fs = require('fs');
+
+fs.readFile('input.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error', err);
+        return;
+    }
+
+    const updatedData = data.replace(/Node/g, 'NODE.JS');
+
+    fs.writeFile('output.txt', updatedData, (err) => {
+        if (err) {
+            console.error('Error', err);
+        } else {
+            console.log('Done!');
+        }
+    });
+});
