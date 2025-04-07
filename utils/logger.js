8ee86177e16273534ad3468a6367a69b43ae098e
TANINSAMXI <pischaniyd@gmail.com>
2025-04-07 10:31:09 +0200

    hw_2 complete

--- utils/logger.js
+++ utils/logger.js
@@ -1,4 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+const logFilePath = path.join(__dirname, '..', 'request.log');
+
 module.exports = function logger(req) {
     const now = new Date().toISOString();
-    console.log(`[${now}] ${req.method} ${req.url}`);
-  };
+    const message = `[${now}] ${req.method} ${req.url}`;
+
+    console.log(message);
+
+    fs.appendFile(logFilePath, message + '\n', err => {
+        if (err) console.error('Помилка запису в лог:', err.message);
+    });
+};
