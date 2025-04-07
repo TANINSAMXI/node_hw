1ffbe33b7053d053fcba23ceec10e93a930b6b92
TANINSAMXI <pischaniyd@gmail.com>
2025-04-07 15:59:00 +0200

    hw_9 complete

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-const message = "Welcome to Node.js"
-alert(message)
+const fs = require('fs');
+
+const readStream = fs.createReadStream('log.txt', {
+    encoding: 'utf8',
+    highWaterMark: 128
+});
+
+readStream.on('data', (chunk) => {
+    console.log('new chunk');
+    console.log(chunk);
+});
+
+readStream.on('end', () => {
+    console.log('end');
+});
+
+readStream.on('error', (err) => {
+    console.error('Error reading file:', err);
+});
