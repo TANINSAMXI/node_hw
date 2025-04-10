5c6022522cdd6c7a0f530609a7ca0e8345a95e09
TANINSAMXI <pischaniyd@gmail.com>
2025-04-10 18:57:31 +0200

    hw_11 complete

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-const message = "Welcome to Node.js"
-alert(message)
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    const { url, method } = req;
+
+    res.setHeader('Content-Type', 'text/plain');
+
+    if (method === 'GET') {
+        if (url === '/') {
+            res.statusCode = 200;
+            res.end('Home Page');
+        } else if (url === '/about') {
+            res.statusCode = 200;
+            res.end('About Us');
+        } else {
+            res.statusCode = 404;
+            res.end('404 Not Found');
+        }
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
