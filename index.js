fe44fc6fcad1a4466b1dee06f0d0d10a5b9a7af6
TANINSAMXI <pischaniyd@gmail.com>
2025-04-14 20:15:05 +0200

    hw_13 complete

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-const message = "Welcome to Node.js"
-alert(message)
+const express = require('express');
+const app = express();
+const port = 3000;
+app.set('view engine', 'pug');
+app.set('views', './views');
+const users = [
+    { name: 'A', age: 25, email: 'a@example.com' },
+    { name: 'B', age: 26, email: 'b@example.com' },
+    { name: 'C', age: 25, email: 'c@example.com' },
+    { name: 'D', age: 30, email: 'd@example.com' },
+    { name: 'E', age: 45, email: 'e@example.com' },
+    { name: 'F', age: 30, email: 'f@example.com' },
+    { name: 'G', age: 29, email: 'g@example.com' },
+    { name: 'H', age: 31, email: 'h@example.com' },
+    { name: 'I', age: 22, email: 'i@example.com' }
+];
+
+app.get('/', (req, res) => {
+    res.render('users', { users });
+});
+
+// Запуск сервера
+app.listen(port, () => {
+    console.log(` http://localhost:${port}`);
+});
