71702f1b20088df1960a3ea37a6f8ab91cf98ec1
TANINSAMXI <pischaniyd@gmail.com>
2025-04-15 18:56:12 +0200

    hw_14 complete

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-const message = "Welcome to Node.js"
-alert(message)
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+const products = [
+    { name: 'A', inStock: true },
+    { name: 'B', inStock: false },
+    { name: 'C', inStock: true },
+    { name: 'D', inStock: false }
+];
+
+app.get('/', (req, res) => {
+    res.render('products', { products });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running at http://localhost:3000');
+});
