b7a0df06e0db20c4a1e14a9ce755c3cd73f73f1a
TANINSAMXI <pischaniyd@gmail.com>
2025-04-16 19:59:27 +0200

    hw_15 complete

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-const message = "Welcome to Node.js"
-alert(message)
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const app = express();
+const port = 3000;
+
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+const users = [
+    { name: 'A', age: 1, email: 'a@example.com' },
+    { name: 'B', age: 2, email: 'b@example.com' },
+    { name: 'C', age: 3, email: 'c@example.com' },
+];
+
+app.get('/', (req, res) => {
+    res.render('users.njk', { users });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
