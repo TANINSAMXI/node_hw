0c6cd6a22c08548997fbbf2c54b469234b0c6fa2
TANINSAMXI <pischaniyd@gmail.com>
2025-04-18 21:16:21 +0200

    hw_17 complete

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-const message = "Welcome to Node.js"
-alert(message)
+const fs = require('fs');
+const path = require('path');
+const buffer = Buffer.from('hillel', 'utf-8');
+const filePath = path.join(__dirname, 'binary-file.bin');
+
+fs.writeFile(filePath, buffer, (writeErr) => {
+    if (writeErr) {
+        console.error('Error writing binary file:', writeErr);
+        return;
+    }
+
+    console.log('Binary file with "hillel" created.');
+
+    fs.readFile(filePath, (readErr, data) => {
+        if (readErr) {
+            console.error('Error reading binary file:', readErr);
+            return;
+        }
+        console.log('\n Buffer content:', data);
+        console.log('\n Text from binary:', data.toString());
+        console.log('\n Hex bytes:');
+        for (let i = 0; i < data.length; i++) {
+            process.stdout.write(data[i].toString(16).padStart(2, '0') + ' ');
+        }
+        console.log();
+    });
+});
