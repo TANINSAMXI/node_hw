b0801b5800e8e9144f010516261fa497ea7c3386
TANINSAMXI <pischaniyd@gmail.com>
2025-04-19 21:40:42 +0200

    hw_18 complete

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const message = "Welcome to Node.js"
-alert(message)
+const message = "Hillel";
+
+const buffer = Buffer.from(message, 'utf8');
+console.log("Binary (hex):", buffer.toString('hex'));
+console.log("Binary (base64):", buffer.toString('base64'));
+
+const restoredMessage = buffer.toString('utf8');
+console.log("Restored text:", restoredMessage);
