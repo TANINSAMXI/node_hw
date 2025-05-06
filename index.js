ec6c855c5ecac33d019250831dbcf2ef07fe39ee
TANINSAMXI <pischaniyd@gmail.com>
2025-05-06 20:27:12 +0200

    hw_28 complete

--- index.js
+++ index.js
@@ -5,13 +5,10 @@ const courseRoutes = require('./routes/courseRoutes');
 const enrollmentRoutes = require('./routes/enrollmentRoutes');
 
 app.use(express.json());
-
-// Роути
 app.use('/api/students', studentRoutes);
 app.use('/api/courses', courseRoutes);
 app.use('/api/enrollments', enrollmentRoutes);
 
-// Запуск сервера
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
